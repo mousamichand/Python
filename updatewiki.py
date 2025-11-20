b8707e7bd46cbe929f1cfaaf039f3c3725941dfe
Mousami <39524207+mousamichand@users.noreply.github.com>
2025-11-20 16:32:58 +0000

    Update updatewiki.py

--- updatewiki.py
+++ updatewiki.py
@@ -1,24 +1,46 @@
 import requests
+import urllib3
 
-def upload_report_to_morpheus(report_file):
-    """
-    Uploads the generated Weblogic IQ/OQ report as a wiki page in Morpheus under the WebLogic section.
-    """
-    url = "https://stage.morpheusdata.com//api/wiki/pages"
-    headers = {
-        "accept": "application/json",
-        "content-type": "application/json"
-       "authorization": "Bearer "+
-    }
-    with open(report_file, "r") as rf:
-        wiki_body = rf.read()
-    payload = {
-        "page": {
-            "name": "Weblogic IO/OQ",
-            "category": "WebLogic",
-            "body": wiki_body
-        }
-    }
+# Disable SSL warnings (optional)
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-    response = requests.post(url, json=payload, headers=headers)
-    print(response.text)
+token = morpheus['morpheus']['apiAccessToken']
+url = "https://stage.morpheusdata.com/api/wiki/pages"
+
+headers = {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+    "Authorization": "Bearer " + token
+}
+
+wiki_body = (
+"**A) REQUEST DETAILS**\n"
+"---------------\n\n"
+"**REQUEST INFO**\n"
+"| **KEY** | **VALUE** |\n"
+"| ------- | --------- |\n"
+"| **REQUESTOR:** | Ximena Rodriguez / <anaximena.rodriguez@abc.om> |\n"
+"| **IDMD:** | POC |\n"
+"| **BUSINESS SERVICE:** | ECS AUTOMATION SERVICE |\n"
+"| **APPLICATION:** | MORPHEUS (PRD) |\n\n"
+"**SERVER INFO**\n"
+"| **KEY** | **VALUE** |\n"
+"| ------- | --------- |\n"
+"| **SERVER NAME:** | bss7b1.abc.com |\n"
+"| **SERVER LOCATION:** | Americas/BR/Cotia |\n"
+)
+
+payload = {
+    "page": {
+        "name": "Weblogic IO/OQ",
+        "category": "WebLogic",
+        "format": "markdown",
+        "body":  wiki_body
+    }
+}
+
+response = requests.post(url, json=payload, headers=headers, verify=False)
+
+print(response.status_code)
+print(response.text)
+
